4fd4587d806ff23cb842cdc8906dbce76941c04b
Sergio Oliveira <sergiomos2010@gmail.com>
2024-10-20 22:54:42 -0300

    add constraints

--- projeto.sql
+++ projeto.sql
@@ -2,58 +2,62 @@
 DROP DATABASE IF EXISTS spotify
 CREATE DATABASE spotify;
 
-CREATE TABLE musics (
- id int NOT NULL,
- title varchar(100) NOT NULL,
- duration NOT NULL,
- PRIMARY KEY (id)
-)
-
 CREATE TABLE singers (
- id int NOT NULL,
- name varchar(255) NOT NULL,
- birthday NOT NULL,
- PRIMARY KEY (id)
- 
+ singer_id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(255) NOT NULL,
+ birthday DATE NOT NULL,
+
 )
 
 CREATE TABLE discs (
- id int NOT NULL,
- title varchar(100) NOT NULL,
- launch_date NOT NULL,
- singer_id int NOT NULL,
- PRIMARY KEY (id)
+ disc_id INT PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(255) NOT NULL,
+ launch_date DATE NOT NULL,
+ singer_id INT NOT NULL,
+
+ FOREIGN KEY (singer_id) REFERENCES singers(singer_id)
 )
 
-CREATE TABLE users (
- id int NOT NULL,
- name varchar(100) NOT NULL,
- email varchar(100) NOT NULL,
- register_date int NOT NULL,
- PRIMARY KEY (id
-)
+CREATE TABLE musics (
+ music_id INT PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(255) NOT NULL,
+ duration INT NOT NULL,
+ disc_id INT,
 
-CREATE TABLE playlists (
- id int NOT NULL,
- title varchar(100) NOT NULL,
- user_id int NOT NULL,
- PRIMARY KEY (id
-)
-
-CREATE TABLE disc_music (
- disc_id int NOT NULL,
- music_id int NOT NULL,
- PRIMARY KEY (id
-)
-
-CREATE TABLE playlist_music (
- playlist_id int NOT NULL,
- music_id int NOT NULL,
- PRIMARY KEY (id
+ FOREIGN KEY (disc_id) REFERENCES discs(disc_id)
 )
 
 CREATE TABLE music_singer (
- singer_id int NOT NULL,
- music_id int NOT NULL,
- PRIMARY KEY (id
+ singer_id INT NOT NULL,
+ music_id INT NOT NULL,
+
+ PRIMARY KEY (singer_id, music_id),
+ FOREIGN KEY (music_id) REFERENCES musics(music_id),
+ FOREIGN KEY (singer_id) REFERENCES singers(singer_id)
 )
+
+CREATE TABLE users (
+ user_id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(255) NOT NULL,
+ email VARCHAR(255) NOT NULL UNIQUE,
+ register_date DATE NOT NULL,
+)
+
+CREATE TABLE playlists (
+ playlist_id INT PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(100) NOT NULL,
+ user_id INT NOT NULL,
+
+ FOREIGN KEY (user_id) REFERENCES users(user_id)
+)
+
+CREATE TABLE playlist_music (
+ playlist_id INT NOT NULL,
+ music_id INT NOT NULL,
+
+ PRIMARY KEY (playlist_id, music_id)
+ FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id)
+ FOREIGN KEY (music_id) REFERENCES musics(music_id)
+)
+
+
